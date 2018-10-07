60283cb6dfbbcebe663f1279559a6eed30d27ef3
Thea Flowers <thea@thea.codes>
2018-10-07 00:57:30 -0700

    Somewhat working new timing protocol

--- ym2612.cpp
+++ ym2612.cpp
@@ -19,16 +19,15 @@ namespace ym2612 {
 #define YM_WR 29
 // Pin 29 to YM CS
 #define YM_CS 28
-// Pin 28 to YM RD (unused)
+// Pin 28 to YM RD
 #define YM_RD 30
 // How long to hold the WR and CS lines when writing data.
 // The datasheet indicates this should be *at least* 200nS, but it appears
 // that a setting of (200) ~2,000uS ensures that the chip always gets our messages.
 #define YM_WRITE_WAIT 11 // * 10uS
-// How long to hold the data lines after pulling the WR and CS lines.
-// The datasheet indicates this should be *at least* 20ns, but it appears
-// that a setting of 100 (~1,000uS) ensures that the chip always gets our messages.
-#define YM_DATA_WAIT 11 // * 10uS
+// The maximum about of uC cycles to wait for the YM2612 to be ready to receive data.
+// This is around 10,000uS.
+#define YM_MAX_WAIT_CYCLES 10000
 // How long to hold the reset line.
 // According to bus timing documents, we must hold the line low for at least
 // 192 cycles. At 8hmz, that's 24 microseconds.
@@ -66,41 +65,121 @@ void reset() {
 }
 
 
+inline static void input_enable() {
+  for (int i = 0; i < 8; i++) {
+    pinMode(YM_DATA + i, INPUT);
+  }
+}
+
+inline static void output_enable() {
+  for (int i = 0; i < 8; i++) {
+    pinMode(YM_DATA + i, OUTPUT);
+  }
+}
+
+
+inline static uint8_t read_data_lines()
+{
+  uint8_t out = 0;
+  for (int i = 0; i < 8; i++){
+    out |= digitalReadFast(YM_DATA + i) << i;
+  }
+  return out;
+}
+
+
 inline static void set_data_lines(byte b)
 {
   /* Sets the data lines to the YM to the given byte. */
-  for (int i = 0; i < 8; i++)
-  {
+  for (int i = 0; i < 8; i++){
     digitalWriteFast(YM_DATA + i, ((b >> i) & 1));
   }
 }
 
+
+inline static void wait_ready(){
+  // // write the address (any address works, so we'll write 0xEE) and enable the
+  // // chip to start the read cycle.
+  // digitalWriteFast(YM_A0, LOW);
+  // output_enable();
+  // set_data_lines(0xEE);
+  // digitalWriteFast(YM_RD, LOW);
+  // digitalWriteFast(YM_CS, LOW);
+
+  // // We must wait for the chip to finish reading our address before trying to read.
+  // delay10ns(YM_WRITE_WAIT);
+  // digitalWriteFast(YM_CS, HIGH);
+
+  // Switch data bus to input YM -> uC.
+  input_enable();
+
+  // Switch A0 to inactive to read the data byte, pull CS and RD low again.
+  digitalWriteFast(YM_WR, HIGH);
+  digitalWriteFast(YM_A1, LOW);
+  digitalWriteFast(YM_A0, LOW);
+  digitalWriteFast(YM_RD, LOW);
+  delay10ns(YM_WRITE_WAIT);
+  digitalWriteFast(YM_CS, LOW);
+  delay10ns(YM_WRITE_WAIT);
+
+  // Wait until the bus' 7th bit is 0.
+  int count = 0;
+  uint8_t state = 0;
+  while(count < YM_MAX_WAIT_CYCLES) {
+    __asm__ volatile ("nop");
+    count++;
+    if(count < 20) continue;
+    state = read_data_lines();
+    if((state & (1 << 7)) == 0) break;
+  }
+
+  // Disable the chip and read pins.
+  digitalWriteFast(YM_RD, HIGH);
+  digitalWriteFast(YM_CS, HIGH);
+
+  // Switch data bus back to ouput. uC -> YM.
+  output_enable();
+
+  // DEBUG
+  Serial.printf("Waited %i\n", count);
+  if(count == YM_MAX_WAIT_CYCLES){
+    Serial.printf("Warning, waited too many cycles for ready, last state: %x, %x, %i.\n", state, (1 << 7), state & (1 << 7));
+  }
+}
+
 void set_reg(uint8_t address, uint8_t data, int port) {
+  // Wait for any previous writes to finish before writing.
+  wait_ready();
+
   // Write the register address first.
   digitalWriteFast(YM_A1, port);
   digitalWriteFast(YM_A0, LOW);
   set_data_lines(address);
+
   // technically we should give *some* time between setting the bytes and
   // pulling the pins, but it seems to work fine without that.
   digitalWriteFast(YM_CS, LOW);
   digitalWriteFast(YM_WR, LOW);
+
+  // We must wait for the chip to finish reading our address before loading in the
+  // next byte.
   // We shouldn't raise the WR and CS until at least YM_WRITE_WAIT have passed.
   delay10ns(YM_WRITE_WAIT);
   digitalWriteFast(YM_WR, HIGH);
   digitalWriteFast(YM_CS, HIGH);
-  // We must wait for the chip to finish reading our data before loading in the
-  // next byte.
-  delay10ns(YM_DATA_WAIT);
 
   // Same as above, but with A0 high to write the register data.
   digitalWriteFast(YM_A0, HIGH);
   set_data_lines(data);
+  delay10ns(YM_WRITE_WAIT);
   digitalWriteFast(YM_CS, LOW);
   digitalWriteFast(YM_WR, LOW);
   delay10ns(YM_WRITE_WAIT);
+
+  // Disable the chip and write pins.
   digitalWriteFast(YM_WR, HIGH);
   digitalWriteFast(YM_CS, HIGH);
-  delay10ns(YM_DATA_WAIT);
+  delay10ns(YM_WRITE_WAIT);
 }
 
 void set_reg(uint8_t address, uint8_t data){
@@ -128,17 +207,14 @@ void set_channel_freq(int channel, float freq){
   // Note this presently turns the key off then back on, when it comes time
   // to implement modulation/pitch bend I'll need to fix this.
   set_reg(0x28, key_offset); // Key off
-  delay(3);
   set_reg(
     0xA4 + channel_offset,
     (block << 3) | (freq_int >> 8),
     port); // freq
-  delay(3);
   set_reg(
     0xA0 + channel_offset,
     freq_int & 0xFF,
     port);
-  delay(3);
   set_reg(0x28, 0xF0 | key_offset); // Key on
 }
 
@@ -165,32 +241,26 @@ void ChannelPatch::write_to_channel(uint8_t channel, ChannelPatch::WriteOption o
 
     if(option == WriteOption::ALL or option == WriteOption::OP0_DT1 or option == WriteOption::OP0_MUL) {
       set_reg(0x30 + operator_offset + channel, dt1_mul_byte, port);
-      delay(2);
     }
 
     if(option == WriteOption::ALL or option == WriteOption::OP0_TL) {
       set_reg(0x40 + operator_offset + channel, tl_byte, port);
-      delay(2);
     }
 
     if(option == WriteOption::ALL or option == WriteOption::OP0_RS or option == WriteOption::OP0_AR) {
       set_reg(0x50 + operator_offset + channel, rs_ar_byte, port);
-      delay(2);
     }
 
     if(option == WriteOption::ALL or option == WriteOption::OP0_AM or option == WriteOption::OP0_D1R) {
       set_reg(0x60 + operator_offset + channel, am_d1r_byte, port);
-      delay(2);
     }
 
     if(option == WriteOption::ALL or option == WriteOption::OP0_D2R) {
       set_reg(0x70 + operator_offset + channel, d2r_byte, port);
-      delay(2);
     }
 
     if(option == WriteOption::ALL or option == WriteOption::OP0_D1L or option == WriteOption::OP0_RR) {
       set_reg(0x80 + operator_offset + channel, d1l_rr_byte, port);
-      delay(2);
     }
   }
 
@@ -199,12 +269,10 @@ void ChannelPatch::write_to_channel(uint8_t channel, ChannelPatch::WriteOption o
 
   if(option == WriteOption::ALL or option == WriteOption::FEEDBACK or option == WriteOption::ALGORITHM) {
     set_reg(0xB0 + channel, feedback_algorithm_byte, port);
-    delay(2);
   }
 
   if(option == WriteOption::ALL) {
     set_reg(0xB4 + channel, 0xC0, port); // Enable output on both speakers (for now)
-    delay(2);
   }
 };
 
